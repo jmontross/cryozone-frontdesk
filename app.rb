4448fe3351a2dcfb14176ee796fd407f9547eb53
Joshua Montross <joshua.montross@gmail.com>
2015-05-28 07:34:15 -0700

    changes

--- app.rb
+++ app.rb
@@ -107,6 +107,7 @@ headers = {
   end
   # logger.info(response.inspect)
   logger.info(people.inspect)
+  logger.info("people.inspect complete")
   # logger.info(response.class.name)
   # "code: #{@code}... token: #{@token.inspect}"
   erb :menu, locals: {people:  people, response: body} 
