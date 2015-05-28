d9926b234fdc3319a8889d58c1a5f635e1f4ec21
Joshua Montross <joshua.montross@gmail.com>
2015-05-28 09:51:07 -0700

    changes

--- app.rb
+++ app.rb
@@ -116,7 +116,7 @@ headers = {
   logger.info("people.inspect complete")
   # logger.info(response.class.name)
   # "code: #{@code}... token: #{@token.inspect}"
-  erb :menu, locals: {people:  people, response: body} 
+  erb :menu, locals: {people:  people.flatten, response: body} 
 end
 
 get '/all_customers' do
